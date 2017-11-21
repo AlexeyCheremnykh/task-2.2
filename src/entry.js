bed456edd1f5fe774a2b333992071b4d7a2b8568
AlexeyCheremnykh <chmnkh@gmail.com>
2017-11-21 23:38:04 +0700

    Кнопки+hover

---
+++ src/entry.js
@@ -0,0 +1,2 @@
+import "./blocks/standard-button-big/standard-button-big.styl"
+import "./blocks/standard-button-small/standard-button-small.styl"
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.styl$/,                
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]                  
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.pug$/,
+                use: ['html-loader', 'pug-html-loader']
+            }
+        ]
+    },
+    plugins: [        
+        new HtmlWebpackPlugin({
+            template: 'src/blocks/index.pug'
+        })
+    ]
+};
