5fa22ca8da27077076ba6f2476bc38812ec1ea00
AlexeyCheremnykh <chmnkh@gmail.com>
2017-11-22 00:16:38 +0700

    Кнопочки готовы (без шрифтов)

--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,3 @@
 import "./blocks/standard-button-big/standard-button-big.styl"
 import "./blocks/standard-button-small/standard-button-small.styl"
+import "../node_modules/css-ripple-effect/dist/ripple.min.css"
