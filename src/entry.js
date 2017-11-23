8bce185e436b85a27aa8bb87111051a9bd01558c
AlexeyCheremnykh <chmnkh@gmail.com>
2017-11-23 13:05:47 +0700

    Standard buttons почти готов

--- src/entry.js
+++ src/entry.js
@@ -1,3 +1,5 @@
+import "./blocks/main-container/main-container.styl"
+import "./blocks/standard-buttons/standard-buttons.styl"
 import "./blocks/standard-button-big/standard-button-big.styl"
 import "./blocks/standard-button-small/standard-button-small.styl"
 import "../node_modules/css-ripple-effect/dist/ripple.min.css"
