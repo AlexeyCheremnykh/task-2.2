2a1a842f8da263a1bf0817746051abdcc83a6df9
AlexeyCheremnykh <chmnkh@gmail.com>
2017-11-23 17:08:54 +0700

    +arrow buttons

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,8 @@
+import "./fonts/Lato Black/fonts.css"
 import "./blocks/main-container/main-container.styl"
 import "./blocks/standard-buttons/standard-buttons.styl"
 import "./blocks/standard-button-big/standard-button-big.styl"
 import "./blocks/standard-button-small/standard-button-small.styl"
+import "./blocks/arrow-button/arrow-button.styl"
+import "./blocks/arrow-buttons/arrow-buttons.styl"
 import "../node_modules/css-ripple-effect/dist/ripple.min.css"
