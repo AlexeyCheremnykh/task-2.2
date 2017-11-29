9464352fcc03b55cc32b0e0a06c66952b1af49de
AlexeyCheremnykh <chmnkh@gmail.com>
2017-11-29 13:43:35 +0700

    Половина макета - ок

--- src/blocks/send-message/send-message.js
+++ src/blocks/send-message/send-message.js
@@ -1,9 +1,25 @@
-/*$(document).ready(function() {
-    $(".send-message").submit(function(event) {
-        $(".send-message .standard-button").click(function() {
-            if (!$(".send-message__input").valid()) {
-                
+$(document).ready(function() {
+    $(".send-message .standard-button").click(function(event) {        
+        $(".send-message").validate({
+            errorPlacement: function (error, element) {            
+            }                    
+        });                 
+        $("input").each(function() {
+            if ($(this).valid()) {            
+                $(".send-message__validation-message", $(this).parent())
+                    .text("THANKS!")
+                    .removeClass("send-message__validation-message_error")
+                    .animate({
+                        opacity: 1
+                    }, 700)
+            } else {
+                $(".send-message__validation-message", $(this).parent())
+                    .text("ERROR!")
+                    .addClass("send-message__validation-message_error")
+                    .animate({
+                        opacity: 1
+                    }, 700)
             }
-        }) 
+        });
     })
-})*/
+})
