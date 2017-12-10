190d449edb3e9870fa2ff25d6ab7de92fbd279a3
AlexeyCheremnykh <chmnkh@gmail.com>
2017-12-10 16:02:49 +0700

    Revert "4revert"
    This reverts commit 37ecb3c4c7540ade2e2228bb5f5e4b27d4d914c9.

--- src/blocks/percentage/percentage.js
+++ src/blocks/percentage/percentage.js
@@ -1,28 +1,13 @@
 $(document).ready(function () {    
     $(".percentage").each(function() {       
         $(this).circleProgress({
-            value: $(this).data("value") / 100,           
+            value: parseInt($(".percentage__value", this).text()) / 100,        
+            size: 95,
             fill: "#e75735",
-            thickness: $(".percentage canvas").width() / 19,
+            thickness: 6,
             emptyFill: "rgba(255, 255, 255, 1)",
             startAngle: -Math.PI / 2,
             insertMode: "prepend"
         })
-       
-        //value text
-        $(this).on('circle-animation-progress', function (e, p, v) {
-            var obj = $(this).data('circle-progress'),
-                ctx = obj.ctx,
-                s = obj.size,
-                sv = (100 * v).toFixed();                
-
-            ctx.save();
-            ctx.font = "300 2.5rem Lato";
-            ctx.textAlign = 'center';
-            ctx.textBaseline = 'middle';
-            ctx.fillStyle = "#9d9d9d";
-            ctx.fillText(sv, s / 2, s / 2);
-            ctx.restore();
-        });       
     });
 });
