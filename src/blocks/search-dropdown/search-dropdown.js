52d3b9b28873c79f0a6c158c97daaf3b0a291d6d
AlexeyCheremnykh <chmnkh@gmail.com>
2018-01-27 13:02:58 +0700

    Добавил/изменил код для миксов; где-то сделал деструктуризацию

--- src/blocks/search-dropdown/search-dropdown.js
+++ src/blocks/search-dropdown/search-dropdown.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     $($(".search__field").get(1))
         .addClass("search__field_not-found")
-        .attr("placeholder", "I've not found what I'm looking for...")
         .click(function() {
             $(this)
                 .removeClass("search__field_not-found")
