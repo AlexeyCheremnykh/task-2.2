919c715283d2d03f64991285cf4d7c75b93036f3
AlexeyCheremnykh <chmnkh@gmail.com>
2018-01-28 16:15:22 +0700

    slider сделал норм контейнер; после этого пофиксил макет

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
     $(".slider").each(function() {
-        $(".slider_type-a")
+        $(".slider_type-a .slider__line")
             .slider({
                 min: $(this).data("min"),
-                max: $(this).data("max"),            
+                max: $(this).data("max"),
             })
             .slider("float");
 
-        $(".slider_type-b")
+        $(".slider_type-b .slider__line")
             .slider({
-                range: "min",            
+                range: "min",
                 min: $(this).data("min"),
                 max: $(this).data("max"),
-                step: $(this).data("step"),            
-                slide: function () {            
-                    $(".ui-slider-range", this).css("background", "#4eb7a8");         
+                step: $(this).data("step"),
+                slide: function () {
+                    $(".ui-slider-range", this).css("background", "#4eb7a8");
                 }
             })
             .slider("pips", {
-                rest: "label"              
+                rest: "label"
             });
     })
 });
