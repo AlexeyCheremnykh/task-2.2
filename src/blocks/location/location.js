61dff508668dd3947535a21d6b22d3a48965e2e1
AlexeyCheremnykh <chmnkh@gmail.com>
2018-02-04 21:13:06 +0700

    location: сделал класс и переименовал 1 элемент

--- src/blocks/location/location.js
+++ src/blocks/location/location.js
@@ -1,15 +1,32 @@
-$(document).ready(function() {
-    $(".location__map")
-        .gmap3({            
-            address: $(".location__map").data("address"),
-            zoom: 14            
-        })
-        .marker({
-            address: $(".location__map").data("address"),
-            icon: {
-                url: "img/map-marker-icon.png",                
-                anchor: new google.maps.Point(20, 56)
-            },
-            draggable: true                       
-        })
+/* global google */
+
+class Map {
+  constructor(elem, id) {
+    this._id = id;
+    this._$map = $(elem);
+  }
+
+  initElement() {
+    this._$map
+      .gmap3({
+        address: this._$map.data('address'),
+        zoom: 14,
+      })
+      .marker({
+        address: this._$map.data('address'),
+        icon: {
+          url: 'img/map-marker-icon.png',
+          anchor: new google.maps.Point(20, 56),
+        },
+        draggable: true,
+      });
+  }
+}
+
+const $map = $('.location__map');
+let id = 0;
+$map.each((index, elem) => {
+  const map = new Map(elem, id);
+  map.initElement();
+  id += 1;
 });
