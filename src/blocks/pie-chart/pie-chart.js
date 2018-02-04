23de557b035291a72d2d8e2ad04f9a49228fdd69
AlexeyCheremnykh <chmnkh@gmail.com>
2018-02-04 22:51:22 +0700

    pie-chart: переписал js

--- src/blocks/pie-chart/pie-chart.js
+++ src/blocks/pie-chart/pie-chart.js
@@ -1,31 +1,58 @@
-var ctx = $(".pie-chart__chart");
-var pc = $(".pie-chart");
-var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    showTooltips: false,
-    data: {        
+/*  global Chart */
+/*  eslint no-unused-vars: 0 */
+
+
+class PieChart {
+  constructor(elem, id) {
+    this._id = id;
+    this._$pieChart = $(elem);
+    this._$canvas = $(elem).children('.js-pie-chart__canvas');
+  }
+
+  initElement() {
+    const myDoughnutChart = new Chart(this._$canvas, {
+      type: 'doughnut',
+      showTooltips: false,
+      data: {
         datasets: [{
-            data: [pc.data("val1"), pc.data("val2"), pc.data("val3"), pc.data("val4")],
-            backgroundColor: [                
-                "#747474",
-                "#e75735",
-                "#4eb7a8",
-                "#e5e5e5"
-            ],
-            borderWidth: 0            
-        }] 
-    },
-    options: {
+          data: [
+            this._$pieChart.data('val1'),
+            this._$pieChart.data('val2'),
+            this._$pieChart.data('val3'),
+            this._$pieChart.data('val4'),
+          ],
+          backgroundColor: [
+            '#747474',
+            '#e75735',
+            '#4eb7a8',
+            '#e5e5e5',
+          ],
+          borderWidth: 0,
+        }],
+      },
+      options: {
         legend: {
-            display: false
+          display: false,
         },
         cutoutPercentage: 65,
         maintainAspectRatio: false,
-        tooltips: { 
-            enabled: false 
+        tooltips: {
+          enabled: false,
         },
-        hover: { 
-            mode: null 
-        },              
-    }
+        hover: {
+          mode: null,
+        },
+      },
+    });
+
+    return this;
+  }
+}
+
+const $pieChart = $('.js-pie-chart');
+let id = 0;
+$pieChart.each((index, elem) => {
+  const pieChart = new PieChart(elem, id);
+  pieChart.initElement();
+  id += 1;
 });
